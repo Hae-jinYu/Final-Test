fc3ea3f21eac98fc94e32ec880173d74036c8efc
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2022-11-23 00:56:30 +0900

    GemMaker Ver1.1

--- Final-Test/Assets/Scripts/GemMaker.cs
+++ Final-Test/Assets/Scripts/GemMaker.cs
@@ -4,33 +4,50 @@ using UnityEngine;
 
 public class GemMaker : MonoBehaviour
 {
+    //public static GemMaker instance;
     public GameObject gemPrefab;
     GameObject player;
     public int gemCounts = 10;
 
+    //List<GameObject> gems = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("elf demo");
+        for (int i = 0; i < gemCounts; i++)
+        {
+            MakeGems();
+        }
+    }
+    void MakeGems()
+    {
+        float randomX = Random.Range(-59f, 66f);
+        float randomZ = Random.Range(-63f, 63f);
+
+        Debug.Log("Gem 持失");
+
+        if (true)
+        {
+            GameObject GemPrefab = (GameObject)Instantiate(gemPrefab, new Vector3(randomX, 2.6f, randomZ), Quaternion.identity);
+            GemPrefab.transform.parent = transform;
+            //gems.Add(GemPrefab);
+        }
+        for (int i = 0; i < gemCounts; i++)
+        {
+            
+            //GameObject GemPrefab = Instantiate(gemPrefab) as GameObject;
+            
+            //GemPrefab.transform.position = new Vector3(randomX, 2.6f, randomZ);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float randomX = Random.Range(3f, 60f);
-        float randomZ = Random.Range(0f, 58f);
+        
+        
 
-        if (true)
-        {
-            Debug.Log("Gem 持失");
-            for (int i = 0; i < gemCounts; i++)
-            {
-                GameObject GemPrefab = Instantiate(gemPrefab) as GameObject;
-                GemPrefab.transform.parent = transform;
-                GemPrefab.transform.position = new Vector3(randomX, 2.6f, randomZ);
-            }
-            //GameObject GemPrefab = (GameObject)Instantiate(gemPrefab, new Vector3(randomX, 2.6f, randomZ), Quaternion.identity);
-        }
+        
 
     }
 }
